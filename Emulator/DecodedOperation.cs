321bb36f3798c9ae35cf3e8923ceb8ee82b17f5e
ImpendingMoon <theimpendingmoon@gmail.com>
2025-12-26 12:16:48 -0600

    Create Operand class, add to DecodedOperation

--- Emulator/DecodedOperation.cs
+++ Emulator/DecodedOperation.cs
@@ -5,4 +5,7 @@ internal class DecodedOperation
     public uint BaseAddress { get; set; }
     public byte[] Opcode { get; set; } = [];
     public string DisplayString { get; set; } = string.Empty;
+    public Operand? Operand1 { get; set; }
+    public Operand? Operand2 { get; set; }
+    public int OperandSize { get; set; }
 }
---
+++ Emulator/Operand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace im8000emu.Emulator
+{
+    internal class Operand
+    {
+        public Constants.RegisterTargets? Target { get; set; } // Register target, if applicable
+        public uint? Immediate { get; set; } // Immediate value, if applicable
+        public bool Indirect { get; set; } // Fetch operand indirectly from Target or Immediate?
+        public short? Displacement { get; set; } // Signed offset for indirect access
+    }
+}
