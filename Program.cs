3f5c08dd3946584ec50b07f03445229c6bd5c4eb
ImpendingMoon <theimpendingmoon@gmail.com>
2025-12-26 21:53:03 -0600

    Add basic Fetch+Decode, Execute loop

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var memoryBus = new Emulator.MemoryBus();
+        var ioBus = new Emulator.MemoryBus();
+
+        var cpu = new Emulator.CPU(memoryBus, ioBus);
+
+        // Example: Run a loop of only `LD A, A` instructions (opcode 0x00 0x00)
+        for (int i = 0; i < 10; i++)
+        {
+            var operation = cpu.Decode();
+
+            Console.WriteLine($"Executing: [{BitConverter.ToString(operation.Opcode.ToArray())}] {operation.DisplayString}");
+
+            cpu.Execute(operation);
+        }
     }
 }
