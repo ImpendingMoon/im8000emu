fba9ea4d8ed5579326b64b0d79d25733310749d0
ImpendingMoon <theimpendingmoon@gmail.com>
2026-03-20 20:46:41 -0500

    Don't re-evaluate the Config every time it's accessed

--- Config.cs
+++ Config.cs
@@ -6,25 +6,25 @@ internal static class Config
 {
 	private static readonly IConfiguration _configuration = new ConfigurationBuilder()
 		.SetBasePath(AppContext.BaseDirectory)
-		.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+		.AddJsonFile("appsettings.json", false, false)
 		.Build();
 
 	/// <summary>The cost to execute an instruction after fetching</summary>
-	public static int BaseInstructionCost => _configuration.GetValue<int>("Emulator:BaseInstructionCost");
+	public static readonly int BaseInstructionCost = _configuration.GetValue<int>("Emulator:BaseInstructionCost");
 	/// <summary>The cost to read/write from memory once</summary>
-	public static int BusCycleCost => _configuration.GetValue<int>("Emulator:BusCycleCost");
+	public static readonly int BusCycleCost = _configuration.GetValue<int>("Emulator:BusCycleCost");
 	/// <summary>The cost to read/write from an IO device once</summary>
-	public static int IOCycleCost => _configuration.GetValue<int>("Emulator:IOCycleCost");
+	public static readonly int IOCycleCost = _configuration.GetValue<int>("Emulator:IOCycleCost");
 	/// <summary>The cost to move a DWord through the ALU</summary>
-	public static int DWordALUCost => _configuration.GetValue<int>("Emulator:DWordALUCost");
+	public static readonly int DWordALUCost = _configuration.GetValue<int>("Emulator:DWordALUCost");
 	/// <summary>Throw exceptions on illegal emulated system states</summary>
-	public static bool EnableStrictMode => _configuration.GetValue<bool>("Emulator:EnableStrictMode");
+	public static readonly bool EnableStrictMode = _configuration.GetValue<bool>("Emulator:EnableStrictMode");
 	/// <summary>Emulated CPU speed in Hz. Recommended 4,000,000 Hz, max is whatever your CPU can handle</summary>
-	public static int CpuSpeedHz => _configuration.GetValue<int>("Emulator:CpuSpeedHz");
+	public static readonly int CpuSpeedHz = _configuration.GetValue<int>("Emulator:CpuSpeedHz");
 	/// <summary>Target framerate. Recommended 60 FPS. Lower values *might* help with slow emulation.</summary>
-	public static int TargetFramerate => _configuration.GetValue<int>("Emulator:TargetFramerate");
+	public static readonly int TargetFramerate = _configuration.GetValue<int>("Emulator:TargetFramerate");
 	/// <summary>Main memory size in bytes. Min 16,384, max 12,582,912</summary>
-	public static uint MemorySize => _configuration.GetValue<uint>("Emulator:MemorySize");
+	public static readonly uint MemorySize = _configuration.GetValue<uint>("Emulator:MemorySize");
 	/// <summary>VRAM size in bytes. Min 4,096, max 2,097,152</summary>
-	public static uint VideoMemorySize => _configuration.GetValue<uint>("Emulator:VideoMemorySize");
+	public static readonly uint VideoMemorySize = _configuration.GetValue<uint>("Emulator:VideoMemorySize");
 }
